7c97c702eddc8ee5cb1746550983ac13854e5eca
JozwiakPiotr <105681652+JozwiakPiotr@users.noreply.github.com>
2024-04-01 18:20:14 +0200

    Update AuthCodeManager.cs

--- src/IdentityParty.Core/AuthCodeManager.cs
+++ src/IdentityParty.Core/AuthCodeManager.cs
@@ -5,21 +5,46 @@ namespace IdentityParty.Core;
 
 public class AuthCodeManager : IAuthCodeManager
 {
+    private readonly IGrantStore _grantStore;
+    private readonly IAuthorizationCodeIssuer _authCodeIssuer;
+    private readonly IAccessTokenManager _accessTokenManager;
+    private readonly IIdTokenManager _idTokenManager;
     public AuthCodeManager(
         IGrantStore grantStore,
-        IAuthorizationCodeIssuer authCodeIssuer
+        IAuthorizationCodeIssuer authCodeIssuer,
+        IAccessTokenManager accessTokenManager,
+        IIdTokenManager idTokenManager
     )
     {
-
+        _grantStore = grantStore;
+        _authCodeIssuer = authCodeIssuer;
+        _accessTokenManager = accessTokenManager;
+        _idTokenManager = idTokenManager;
     }
 
-    public Task<string> AssignAuthCodeAsync(Grant grant)
+    public async Task<string> AssignAuthCodeAsync(Grant grant)
     {
-        throw new NotImplementedException();
+        var code = _authCodeIssuer.Issue();
+        grant.AssignAuthCode(code);
+        await _grantStore.UpdateAsync(grant);
+        return code;
     }
 
     public Task<bool> ValidateAuthorizationCodeAsync(string passedCode, Guid clientId)
     {
-        throw new NotImplementedException();
+        var grant = await _grantStore.GetAsync(clientId, passedCode);
+        if(grant is null)
+            return false;
+        if(passedCode != grant.AuthorizationCode)
+            return false;
+        if(grant.WasUsed)
+        {
+            _accessTokenManager.CancelCurrent();
+            _idTokenManager.CancelCurrent();
+            return false;
+        }
+        if(grant.AuthCodeExp < DateTime.Now)
+            return false;
+        return true;
     }
 }
